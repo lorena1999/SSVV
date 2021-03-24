8e910cf0e2f22d441db9329b4a7be42f3623196c
Lorena <lorena.marginean@yahoo.com>
2021-03-24 15:53:44 +0200

    frfui

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,10 +6,7 @@ import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
-import validation.NotaValidator;
-import validation.StudentValidator;
-import validation.TemaValidator;
-import validation.Validator;
+import validation.*;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +14,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // hmm.. eu zic sa faci alta clasa sa vezi daca merge altfel. si tot adaugi pe ea
 // ok
@@ -24,7 +22,7 @@ public class StudentTest {
 
     private Service service;
 
-    public void functie() throws Exception
+    /*public void functie() throws Exception
     {
         Path source = Paths.get("studenti.xml");
         Files.copy(source, source.resolveSibling("studenti_backup.xml"), StandardCopyOption.REPLACE_EXISTING);
@@ -41,7 +39,7 @@ public class StudentTest {
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
-    }
+    }*/
 
 //    @BeforeEach
 //    public void setUp() throws Exception {
@@ -66,156 +64,72 @@ public class StudentTest {
     @Test
     public void testIdUnique()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("3", "Petrica", 500), "If the id is unique the student should be added");
     }
 
     @Test
     public void testIdNotUnique()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(0, service.saveStudent("4", "Petrica", 501), "If the id is NOT unique the student should not be added");
     }
 
     @Test
     public void testIdNotEmpty()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("5", "Petrica", 502), "If the id is NOT empty the student should be added");
     }
-
+//aici pare sa mearga...
     @Test
     public void testIdEmpty()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(0, service.saveStudent("", "Petrica", 503), "If the id is empty the student should NOT be added");
     }
 
     @Test
     public void testNameNotEmpty()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("7", "Petrica", 504), "If the name is NOT empty the student should be added");
     }
 
     @Test
     public void testNameEmpty()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        assertEquals(0, service.saveStudent("8", "", 505), "If the name is empty the student should NOT be added");
+        assertThrows(ValidationException.class, () -> service.saveStudent("8", "", 505), "If the name is empty the student should NOT be added");
     }
 
     @Test
     public void testGroupMinValueMinus1()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(0, service.saveStudent("12", "Petrica", 109), "If the group is lower than min value, the student should NOT be added");
     }
 
     @Test
     public void testGroupMinValue()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("13", "Petrica", 110), "If the group is equal to min value, the student should be added");
     }
 
     @Test
     public void testGroupMinValuePlus1()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("14", "Petrica", 111), "If the group is higher than min value, the student should be added");
     }
 
     @Test
     public void testGroupMaxValueMinus1()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("15", "Petrica", 937), "If the group is lower than max value, the student should be added");
     }
 
     @Test
     public void testGroupMaxValue()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(1, service.saveStudent("16", "Petrica", 938), "If the group is equal to max value, the student should be added");
     }
 
     @Test
     public void testGroupMaxValuePlus1()
     {
-        try {
-            functie();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         assertEquals(0, service.saveStudent("17", "Petrica", 939), "If the group is higher than max value, the student should NOT be added");
     }
 
